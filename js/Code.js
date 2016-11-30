e4bdfba46462d0893bbb599d5aea2b902ae90058
tundelola <foladayo@aol.com>
2016-11-30 15:40:22 -0500

    first commit

---
+++ js/Code.js
@@ -0,0 +1,32 @@
+
+	
+	console.log("Hellow World");
+//var x =5;
+//alert(x+y);
+//var x = prompt("Please enter a number");
+//var y = parseInt(x)
+//console.log(x);
+
+//if(x == 5)
+//	console.log("You got the secret code!");
+//}
+//{
+
+	//console.log("Nice Try");}
+//for loop
+//for (var i = 1; i <=10; i++) {
+
+	//console.log(i);
+//}
+
+function printMessage()
+{
+
+	//alert("Hello World! from Mr. Button");
+	//document.write("<a href =https://www.google.com/>Google</a>");
+	var img = document.getElementByID("googleImage");
+	//img.getElementByID('scr');
+	img.setAttribute('src','images/g.jpg');
+	var par = document.getElementById("parl");
+	par.innerHTML += "Hello all!";
+}
---
+++ js/Code1.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hellow World</title>
+	<script src= "js/code.js" language="javascript" type = "text/javascript">
+	Console.log("Hellow World");
+
+	</script>
+
+	<button type = "button" onclick = "printMessage()">Click Me!</button>
+
+
+	<img id ="googleImage" scr = "images/google.png"/>
+
+</head>
+<body>
+
+</body>
+</html>
